f9d6bc76c08cb9f36c4df7df84ecbb8266185e31
xuxin1955 <x2874836915@qq.com>
2026-05-07 16:04:47 +0800

    update

--- diy-part5.sh
+++ diy-part5.sh
@@ -8,5 +8,6 @@
 # sed -i '/sed -i .*root:::0:.*$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 
-
+# 调试
+sed -i 's|src-git-full openstick https://github.com/lkiuyu/openstick-feeds.git|src-git-full openstick https://github.com/xuxin1955/openstick-feeds|g' feeds.conf.default
 
