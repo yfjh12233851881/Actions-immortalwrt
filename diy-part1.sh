829bc69f62e9586f968333213f921390d62f6f02
xuxin1955 <x2874836915@qq.com>
2026-05-10 02:07:36 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,41 +23,5 @@ sed -i 's|src-git-full openstick https://github.com/lkiuyu/openstick-feeds.git|s
 # src-git-full openstick https://github.com/xuxin1955/openstick-feeds.git
 # EOF
 
-# test
-cat << 'EOF' > target/linux/msm89xx/patches-6.12/999-support-wcn36xx-concurrency.patch
---- a/drivers/net/wireless/ath/wcn36xx/main.c
-+++ b/drivers/net/wireless/ath/wcn36xx/main.c
-@@
-+static const struct wiphy_iface_combination wcn36xx_iface_combos[] = {
-+	{
-+		.limits = {
-+			{ .type = NL80211_IFTYPE_STATION, .max = 1 },
-+			{ .type = NL80211_IFTYPE_AP, .max = 1 },
-+		},
-+		.n_limits = 2,
-+		.max_interfaces = 2,
-+	},
-+};
-+
- static int wcn36xx_init_ieee80211(struct wcn36xx *wcn)
- {
- 	static const u32 cipher_suites[] = {
-@@
- 	wiphy_ext_feature_set(wcn->hw->wiphy,
- 			      NL80211_EXT_FEATURE_CQM_RSSI_LIST);
- 
-+	wcn->hw->wiphy->iface_combinations = wcn36xx_iface_combos;
-+	wcn->hw->wiphy->n_iface_combinations = ARRAY_SIZE(wcn36xx_iface_combos);
-+
- 	return 0;
- }
-EOF
-
-
-
-
-
-
-
 # inspect
 echo ">>> OPP VOLTAGE COUNT: $(grep -c 'opp-microvolt' target/linux/msm89xx/dts/msm8916.dtsi) <<<"
