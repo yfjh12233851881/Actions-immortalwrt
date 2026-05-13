537708d83961856b00346e843ccf0c6001238120
xuxin1955 <x2874836915@qq.com>
2026-05-13 11:42:45 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,10 +8,10 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # temp
-git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
 git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini package/luci-app-cpu-status-mini
-git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 
 # DbusSmsForwardCPlus
 git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
